7819692e47a9f04afe3e1d43bdfb6df376eea2fd
Alex <curjucklloyd@gmail.com>
2023-04-25 23:56:24 +0300

    Added localisation (just for conveniance)

---
+++ src/Locale.py
@@ -0,0 +1,7 @@
+class Locale():
+    menubar_help = "Справка"
+    menubar_about = "О программе..."
+    menubar_aboutqt = "О библиотеке Qt..."
+    
+    mw_name = "Приложуха"
+    mw_about = "Программа база данных тыр пыр"
