222f359f0f7d9fdd30aa5a830c17cff0c39b862f
Alex <curjucklloyd@gmail.com>
2023-04-25 23:58:38 +0300

    Main menu: Help + 2 options

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -1,7 +1,28 @@
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QMessageBox
+from PyQt5.QtCore import pyqtSlot
+from MainMenu import MainMenu
+from Locale import Locale
+l = Locale()
+
 
 class MainWindow(QMainWindow):
 
     def __init__(self, parent=None):
         super().__init__(parent)
-        
+
+        main_menu = MainMenu(parent=self)
+        self.setMenuBar(main_menu)
+
+        main_menu.about_qt.triggered.connect(self.about_qt)
+        main_menu.about.triggered.connect(self.about)
+
+    @pyqtSlot()
+    def about(self):
+        title = l.mw_name
+        text = l.mw_about
+        QMessageBox.about(self, title, text)
+
+    @pyqtSlot()
+    def about_qt(self):
+        QMessageBox.aboutQt(self, l.mw_name)
+
