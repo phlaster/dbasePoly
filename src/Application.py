64b446aa87cd729c99988500ab112d668e4d2203
Alex <curjucklloyd@gmail.com>
2023-05-02 19:50:22 +0300

    QtSql connection created

--- src/Application.py
+++ src/Application.py
@@ -1,7 +1,23 @@
+import sys
+
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtSql import QSqlDatabase
+import settings as st
+
 
 class Application(QApplication):
 
     def __init__(self, argv):
         super().__init__(argv)
         
+        db = QSqlDatabase.addDatabase('QPSQL') # for PostgreSQL
+        db.setHostName(st.db_params['host'])
+        db.setDatabaseName(st.db_params['dbname'])
+        db.setPort(st.db_params['port'])
+        db.setUserName(st.db_params['user'])
+        db.setPassword(st.db_params['password'])
+        
+        if db.open():
+            print(f"Connected to database {db.databaseName()}!", file = sys.stderr)
+        else:
+            print("Connection FAILED", file = sys.stderr)
