14872b997d15f838da073405c8a80f369c118d22
Alex <curjucklloyd@gmail.com>
2023-05-02 19:52:54 +0300

    added window parameters added Employees to menubar

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -1,20 +1,52 @@
 from PyQt5.QtWidgets import QMainWindow, QMessageBox
 from PyQt5.QtCore import pyqtSlot
+
+from Employees import Model, View, Dialog
 from MainMenu import MainMenu
-from Locale import Locale
-loc = Locale()
+
+from settings import Locale as loc
+from settings import mw_params
 
 
 class MainWindow(QMainWindow):
-
     def __init__(self, parent=None):
         super().__init__(parent)
 
+        # Parameters of main window
+        self.setWindowTitle(mw_params["title"])
+        self.setFixedWidth(mw_params["width"])
+        self.setFixedHeight(mw_params["height"])
+
+        # Menubar
         main_menu = MainMenu(parent=self)
         self.setMenuBar(main_menu)
 
+        # slot: Employees
+        main_menu.addEmpl.triggered.connect(self.addEmpl)
+        main_menu.removeEmpl.triggered.connect(self.removeEmpl)
+        main_menu.vacations.triggered.connect(self.vacations)
+
+        # slot: Help
         main_menu.about.triggered.connect(self.about)
         main_menu.about_qt.triggered.connect(self.about_qt)
+
+
+    @pyqtSlot()
+    def addEmpl(self):
+        View()
+        # pass
+
+    @pyqtSlot()
+    def removeEmpl(self):
+        pass
+
+    # Blanc box
+    @pyqtSlot()
+    def vacations(self):
+        title = loc.vacationbox_title
+        text = loc.vacationbox_blanc
+        QMessageBox.about(self, title, text)
+
 
     @pyqtSlot()
     def about(self):
@@ -25,4 +57,3 @@ class MainWindow(QMainWindow):
     @pyqtSlot()
     def about_qt(self):
         QMessageBox.aboutQt(self, loc.helpbox_aboutqt)
-
